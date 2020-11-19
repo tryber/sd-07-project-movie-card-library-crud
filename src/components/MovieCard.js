d503a5479eb446a5b1f94f4d0e8db9bf23a22511
lisvalver <lisvalver.atte@gmail.com>
2020-11-19 20:19:02 -0300

    adding PropTypes to MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
@@ -18,5 +19,14 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
