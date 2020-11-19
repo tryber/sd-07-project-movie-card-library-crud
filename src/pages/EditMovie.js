35c23e15c3395353403b125cfbcea65c25629a59
Strongreen <strongreen@live.com>
2020-11-19 16:58:37 -0300

    Testando se o primeiro requisito foi atendido

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -25,6 +25,7 @@ class EditMovie extends Component {
 
     return (
       <div data-testid="edit-movie">
+        <p>Apenas para teste</p>
         <MovieForm movie={movie} onSubmit={this.handleSubmit} />
       </div>
     );
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -15,6 +15,7 @@ class NewMovie extends Component {
   render() {
     return (
       <div data-testid="new-movie">
+        <p>New Movie</p>
         <MovieForm onSubmit={this.handleSubmit} />
       </div>
     );
