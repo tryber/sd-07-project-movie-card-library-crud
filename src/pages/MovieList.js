4f538ff1b635027c36af6bbef9fd29e86a73f483
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-19 17:08:34 -0300

    CC correction

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -7,40 +7,40 @@ class MovieList extends Component {
   constructor() {
     super();
 
-    this.fetchMovies = this.fetchMovies.bind(this)
+    this.fetchMovies = this.fetchMovies.bind(this);
 
     this.state = {
       movies: [],
-      loading: false
+      loading: false,
     };
   }
-  // List Movies
-  async fetchMovies() {
-    this.setState({ loading: true });
-      const data = await movieAPI.getMovies();
-      this.setState({ movies: data, loading: false });
-  }
-  
+
   componentDidMount() {
     this.fetchMovies();
   }
 
+  // List Movies
+  async fetchMovies() {
+    this.setState({ loading: true });
+    const data = await movieAPI.getMovies();
+    this.setState({ movies: data, loading: false });
+  }
+
   render() {
     const { movies, loading } = this.state;
-    if(loading) {
+    if (loading) {
       return (
         <div>
           <p>Carregando...</p>
         </div>
-      )
-    } else {
-     // Render Loading here if the request is still happening
-      return (
-        <div data-testid="movie-list">
-          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-        </div>
       );
     }
+    // Render Loading here if the request is still happening
+    return (
+      <div data-testid="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
   }
 }
 
