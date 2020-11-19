7484d832ff8bf371c70f4a5aceaf7983587acd88
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-19 20:29:30 -0300

    cc ultimo requisito

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components/index';
 
 import * as movieAPI from '../services/movieAPI';
