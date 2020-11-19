ceb92ad8c0a4385d0391a01fd1b34335d39d7c09
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-19 15:51:17 -0300

    more requisites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id } } = this.props
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{this.props.movie.title}</p>
+        <p>{this.props.movie.storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link  } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super()
+    this.state ={
+      movie: [],
+      loading: true,
+    }
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  async fetchMovie() {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      loading: false
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+    // console.log(this.state.movies);
+    // const getMovie = this.state.movies.find((movie) => movie.id === parseInt(this.props.match.params.id));
+    console.log(this.state.movie);
+    if(this.state.loading) return <Loading />
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
