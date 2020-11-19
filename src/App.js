5e534a20bbd1557aa9153fc823b4869d9c3f972c
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-19 16:59:59 -0300

    corrigindo cc de novo

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-      <Route exact path="/movies/:id/edit" component={EditMovie} />
-      <Route exact path="/movies/new" component={NewMovie} />
-      <Route exact path="/movies/:id" component={MovieDetails} />
-      <Route exact path="/" component={MovieList} />
-      <Route exact path="*" component={NotFound} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="*" component={NotFound} />
     </Switch>
-    </BrowserRouter>
+      </BrowserRouter>
   );
 }
 
