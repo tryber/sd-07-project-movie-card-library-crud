64dcf5479413c84f66611a0d3bf555062c0fcb1d
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-19 17:27:26 -0300

    Corrigindo issue

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import MovieList from './pages/MovieList'
-import MovieDetails from './pages/MovieDetails'
-import NewMovie from './pages/NewMovie'
-import EditMovie from './pages/EditMovie'
-import NotFound from './pages/NotFound'
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={MovieList} />
-          <Route path='/movies/:id' component={MovieDetails} />
-          <Route path='/movies/new' component={NewMovie} />
-          <Route path='/movies/:id/edit' component={EditMovie} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
