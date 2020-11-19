dfc687b7cf10c9527837d3b2bf4c04655d335ebd
Samanta Below <samanta.below@gmail.com>
2020-11-19 18:14:27 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-      <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit" component={EditMovie} />
         <Route path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
