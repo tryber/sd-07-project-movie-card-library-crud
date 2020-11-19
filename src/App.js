2b5464f7ffd8508ebcfb1d8b7a198168219014c6
moacyrrln <moacyrrln@gmail.com>
2020-11-19 16:13:47 -0300

    requisito1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <main>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/" component={MovieList} />
+      </main>
+    </BrowserRouter>
   );
 }
 
