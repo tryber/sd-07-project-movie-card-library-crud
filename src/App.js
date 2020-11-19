28bc59bf8887f9d1e95197044853316823f63f38
Lfocosi <luccafocosi@gmail.com>
2020-11-19 15:17:56 -0300

    rotas setadas cc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index'
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
