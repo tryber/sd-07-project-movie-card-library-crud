bb3be00e56a4aae4c6dd6135f3e3aa9021af2759
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-19 18:44:08 -0300

    corrigido nomenclatura do App

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
+
+function App() {
+  return (
+    <div>
+      Movie Card Library CRUD
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route exact path="movies/:id" component={MovieDetails} />
+        <Route path="movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
