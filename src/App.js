39358838870b2a14b6dc91839ebfd462251d1b1d
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-19 15:28:47 -0300

    Adicionando as rotas para cada pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound,
+} from './pages/index';
+
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
