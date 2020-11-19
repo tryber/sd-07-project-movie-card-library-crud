89dcb2d5af151d48562e875dee1a098af9956ab5
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-19 16:18:20 -0300

    initial commit - requisition 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import { BrowserRouter, Route } from 'react-router-dom';  
 
 function App() {
   return (
+    <BrowserRouter>
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie}  />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route path='*' component={NotFound} />
     <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 
