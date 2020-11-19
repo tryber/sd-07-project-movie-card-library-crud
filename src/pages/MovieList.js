8f2d69d846e6cd6e4f63c7a9faa295cf51d37379
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-19 17:02:20 -0300

    fetchMovies in MovieList, render loading state.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -7,21 +7,40 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMovies = this.fetchMovies.bind(this)
+
     this.state = {
       movies: [],
+      loading: false
     };
+  }
+  // List Movies
+  async fetchMovies() {
+    this.setState({ loading: true });
+      const data = await movieAPI.getMovies();
+      this.setState({ movies: data, loading: false });
+  }
+  
+  componentDidMount() {
+    this.fetchMovies();
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+    const { movies, loading } = this.state;
+    if(loading) {
+      return (
+        <div>
+          <p>Carregando...</p>
+        </div>
+      )
+    } else {
+     // Render Loading here if the request is still happening
+      return (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
+      );
+    }
   }
 }
 
