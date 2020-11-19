f44430d6a90d19ccc38ac69daa554cd2b9d50e8e
Breno Alves <brenoalv3ss@gmail.com>
2020-11-19 19:27:49 -0300

    Ajustes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NewMovie, EditMovie, MovieDetails, MovieList, NotFound } from './pages/index';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Router pach="/movies/new" component={NewMovie} />
-        <Router pach="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
-        <Router pach="/movies/:id" render={(props) => <MovieDetails {...props} />} />
-        <Router exact pach="/" component={MovieList} />
-        <Router component={NotFound} />
+        <Route pach="/movies/new" component={NewMovie} />
+        <Route pach="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route pach="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact pach="/" component={MovieList} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
