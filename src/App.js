87880d71ecc3099e53ed475c24362faa9d34a3aa
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-19 17:05:03 -0300

    Renderizando BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <BrowserRouter />
+      <div>Movie Card Library CRUD</div>
+    </div>
+
   );
 }
 
