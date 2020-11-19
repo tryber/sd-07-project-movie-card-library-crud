aa4435a9d6ad09e9c2553031d3337ed36e8eb98a
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-19 16:47:35 -0300

    verificando novamente finalizaçao

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+import { MovieCard , Loading } from '../components/MovieCard';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-    }
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    this.setState({ loading: true }, async () => {
+      const requestMovies = await movieAPI.getMovies();
+      this.setState({ movies: requestMovies, loading: false });
+    })
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? (
+          <Loading />
+        ) : (
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        )}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import MovieForm from '../components/MovieForm';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      Loading: false,
+      shoudRedirect: false,
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    this.setState({ Loading: true }, async () => {
+      await movieAPI.createMovie(newMovie);
+      this.setState({ Loading: false, shoudRedirect: true });
+    });
   }
 
   render() {
+    const { loading, shoudRedirect } = this.state;
+    if (shoudRedirect) return <Redirect to="/" />;
+
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        {loading ? <loading /> : <MovieForm onSubmit={this.handleSubmit} />}
       </div>
     );
   }
