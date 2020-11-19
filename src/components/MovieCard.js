8a8531a44a1dff431a9cd42220783c72f3070863
gabrieldeori <deori.pessoal@gmail.com>
2020-11-19 17:06:46 -0300

    App Routed | MovieList + Loading Implemented and MovieCard Partial Implemented

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const {imagePath, title, subtitle, storyline, rating} = this.props.movie
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
+        <div className="movie-card-body">
+          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components/index';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +9,42 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loadingText: false,
+    };
+    this.gettingMovies = this.gettingMovies.bind(this);
+    this.screenFunction = this.screenFunction.bind(this);
+  }
+
+  componentDidMount() {
+    this.gettingMovies();
+  }
+
+  async gettingMovies() {
+    this.setState({
+      loadingText: true,
+    });
+    const moviesList = await movieAPI.getMovies();
+    this.setState({
+      movies: moviesList,
+      loadingText: false,
+    });
+    console.log(moviesList);
+  }
+
+  screenFunction(moviesList) {
+    if (!this.state.loadingText) {
+      return moviesList.map((movie) => <MovieCard key={movie.title} movie={movie} />);
     }
+    return <Loading />;
   }
 
   render() {
     const { movies } = this.state;
-
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {this.screenFunction(movies)}
       </div>
     );
   }
