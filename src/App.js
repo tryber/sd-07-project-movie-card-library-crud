d7701dcf289689e807429e3b32f2c9a239a43346
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-19 14:48:58 -0300

    iniciando o projeto. VAMOS COM TUDO

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import{BrowserRouter , Router} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
+    <Router>
     <div>Movie Card Library CRUD</div>
+    </Router>
+    </BrowserRouter>
   );
 }
 
