12b07b315b35268dbe060b7329c30c708d5a50ec
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-11-19 19:25:45 -0300

    fixing cc issues

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { MovieList, MovieDetails, NewMovie, NotFound, EditMovie } from './pages/
 
 function App() {
   return (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route exact path="/" component={MovieList} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
