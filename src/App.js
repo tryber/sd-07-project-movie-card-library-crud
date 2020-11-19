3bb562c41d64a45989dd3395756daf05a7175c98
moises <moisaant@gmail.com>
2020-11-19 15:27:52 -0300

    requisito/1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie } from './pages/index'
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" render={<MovieDetails {...props} />}/>
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" render={<EditMovie {...props} />} />
+    </BrowserRouter>
   );
 }
 
