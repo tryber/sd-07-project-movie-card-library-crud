373a12b81585f242d2695bab376215dd2f130411
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-19 18:07:34 -0300

    corrigindo cc

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import MovieCard from '../components/MovieCard';
-import * as movieAPI from '../services/movieAPI';
+import { MovieCard, Loading } from '../components/MovieCard';
 
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
