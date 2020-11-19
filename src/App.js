b495abf047df2eee68992d4cfcb29f170270f2a4
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-19 19:04:30 -0300

    corrigido caminho do MovieDetails

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route exact path="movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route path="movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
