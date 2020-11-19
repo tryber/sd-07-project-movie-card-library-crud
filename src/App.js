c2966878eb5dc5496db417f8d618c4c77e32c2ee
Renato Silva <renatosdev@gmail.com>
2020-11-19 19:12:10 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -12,12 +12,11 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route path="" component={NotFound} />
       </Switch>
     </BrowserRouter>
