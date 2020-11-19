649ab5f22ed023400cf87f288e33fa9729f4fd90
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-11-19 17:29:23 -0300

    encremento do componente moviecard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <image scr={imagePath} alt="Poster do filme" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <span><Link to={`/movies/${id}`}>VER DETALHES</Link></span>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components/index.js';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
+    this.handleLoading = this.handleLoading.bind(this);
 
     this.state = {
       movies: [],
-    }
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCards();
+  }
+
+  handleLoading() {
+    return this.state.isLoading && <Loading />;
+  }
+
+  fetchCards() {
+    this.setState({ isLoading: true },
+      async () => {
+        const arrayMovies = await getMovies();
+        this.setState({ movies: arrayMovies, isLoading: false });
+      });
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
+        { this.handleLoading() }
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
