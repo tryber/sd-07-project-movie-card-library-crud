df90a30b3f1a659ab311617790372f2b54e181ce
vanderson-henrique <vhbenedito@gmail.com>
2020-11-19 15:07:16 -0300

    Requisito 1 - Definindo Rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  EditMovie,
+  MovieDetails,
+  MovieList,
+  NewMovie,
+  NotFound,
+} from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
