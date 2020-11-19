57da8a9ef0cd3dacbb4d491f4cc3c920c82e4692
Fernando <nando.neves@gmail.com>
2020-11-19 19:30:32 -0300

    third

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
+    const link = `/movies/${this.props.movie.id}`
     return (
       <div data-testid="movie-card">
         Movie Card
+        <p>Título: {this.props.key}</p>
+        <p>Sinopse: {this.props.movie.storyline}</p>
+        <Link to={link}>VER DETALHES</Link>"
       </div>
+
+      // Todos os MovieCards devem possuir em seu conteúdo, pelo menos, o título,
+      //       a sinopse e um link com o texto "VER DETALHES" que aponta para a rota
+      //     movies /: id, onde : id é o id do filme.Esta rota exibirá informações
+      //     detalhadas de um filme.
     );
   }
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,33 +9,30 @@ class MovieList extends Component {
     this.state = {
       movies: [],
       loading: false,
-    }
-  }
-
-  fetchMovies() {
-    this.setState({ loading: true }, async () => {
-      const data = await movieAPI.getMovies()
-      console.log(data)
-      this.setState({ movies: data, loading: false });
-    });
+    };
   }
 
   componentDidMount() {
     this.fetchMovies();
   }
 
+  fetchMovies() {
+    this.setState({ loading: true }, async () => {
+      const data = await movieAPI.getMovies();
+      this.setState({ movies: data, loading: false });
+    });
+  }
   render() {
     const { movies, loading } = this.state;
-    console.log(this.state.movies)
-    let option
+    let option;
     if (loading) {
       option = <p>Carregando...</p>;
     } else {
-      option = <div data-testid="movie-list">
+      option = (<div data-testid="movie-list">
         {movies.map((movie) => (
           <MovieCard key={movie.title} movie={movie} />
         ))}
-      </div>;
+      </div>);
     }
     return (
       <div>
