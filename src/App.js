8b49517ff93f419f75b9ef5e98167cb54e4ac7ad
Carlos Souza <carlos.pessoal@hotmail.com>
2020-11-19 14:54:47 -0300

    Primeiro e segundo requisito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound,
+} from './pages';
+
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
+
+import { MovieCard, Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,23 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-    }
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => this.setState({ movies }));
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {movies.length ? (
+          movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
+        ) : (
+          <Loading />
+        )}
       </div>
     );
   }
