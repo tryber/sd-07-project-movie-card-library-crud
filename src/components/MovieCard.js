84a89d26bead10579fd0a6b23cfbb15b9eab373d
Carlos Souza <carlos.pessoal@hotmail.com>
2020-11-19 15:10:22 -0300

    Terceiro requisito

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <header>
+          <img alt="thumbnail" src={imagePath} />
+          <h2>{title}</h2>
+        </header>
+        <main>
+          <p>{storyline}</p>
+        </main>
+        <footer>
+          <span>
+            <Link to={`/movies/${id}`}>VER DETALHES</Link>
+          </span>
+        </footer>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
