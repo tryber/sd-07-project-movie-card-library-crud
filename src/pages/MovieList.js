23518d4c4f27172df92b75577863e5d1986681c9
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-19 17:10:00 -0300

    Funçao de requisiçao para a MovieList e mensagem de carregamento

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -10,16 +11,37 @@ class MovieList extends Component {
     this.state = {
       movies: [],
     };
+
+    this.renderMovieList = this.renderMovieList.bind(this);
+  }
+
+  async fetchMovies() {
+    const requestMovie = await movieAPI.getMovies();
+    this.setState({
+      movies: requestMovie,
+    });
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  renderMovieList() {
+    const { movies } = this.state;
+
+    return (
+      <div data-testid="movie-list" className="movie-list">
+        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      </div>
+    );
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+      <div>
+        {movies.length === 0 ? <Loading /> : this.renderMovieList()}
       </div>
     );
   }
