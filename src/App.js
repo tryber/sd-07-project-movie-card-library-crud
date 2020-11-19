2114b280aa82d27cb768f63756bc67bc0f749f1a
alvaro <alvarovascon2@gmail.com>
2020-11-19 14:41:44 -0300

    projeto Movie-cards CRUD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="*" component={NotFound} />
+      </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
