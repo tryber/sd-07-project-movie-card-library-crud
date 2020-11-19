2e3c8987d2a3a94de8431307a2d53c1c86fb4196
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-19 17:11:25 -0300

    Conteudo do MovieCard e estilizaçao no App.css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath } = movie;
+
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" className="movie-card">
+        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
       </div>
     );
   }
