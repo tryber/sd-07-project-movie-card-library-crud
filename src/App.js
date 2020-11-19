96096758ebab6023f0b4871c054d4b3501418cf7
Pablo <pablopessanh@gmail>
2020-11-19 14:45:37 -0300

    Starting project; add file  and ignoring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  EditMovie,
+  NotFound,
+} from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <React.Fragment>
+      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="*" component={NotFound} />
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
