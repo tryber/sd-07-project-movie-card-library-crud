224c8013476153877ab4c5164b632952c5b6a515
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-19 16:49:52 -0300

    corrigindo cc

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-  <BrowserRouter>
-   <Switch>
-    <Route exact path="/movies/:id/edit" component={EditMovie} />
-    <Route exact path="/movies/new" component={NewMovie} />
-    <Route exact path="/movies/:id" component={MovieDetails} />
-    <Route exact path="/" component={MovieList} />
-    <Route exact path="*" component={NotFound} />
-   </Switch>
-  </BrowserRouter>
+    <BrowserRouter>
+    <Switch>
+     <Route exact path="/movies/:id/edit" component={EditMovie} />
+     <Route exact path="/movies/new" component={NewMovie} />
+     <Route exact path="/movies/:id" component={MovieDetails} />
+     <Route exact path="/" component={MovieList} />
+     <Route exact path="*" component={NotFound} />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
