f52367915c14fefbc230434f88a7d4e4c761661d
Eduardo Pizorno <pizorno@gmail.com>
2020-11-19 14:28:55 -0300

    iniciando o projeto Movie Card Library CRUD. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Route, Router } from 'react-router-dom';
+import './index.js';
+import './App.css'; 
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Route path="/" component={}></Route>
+      <div>Movie Card Library CRUD</div>
+    </Router>
+    
   );
 }
 
