a2e1fa0a5ef7251e3b05c4fcedae510bf7774bfa
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-11-19 15:44:40 -0300

    Testando correcao rotas 3

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/movies/new" component={NewMovie} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
