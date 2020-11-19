1e71325a5d129ec147be6d97bff2f08b2c940420
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-19 17:33:14 -0300

    Corrigindo método de importação

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index'
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
