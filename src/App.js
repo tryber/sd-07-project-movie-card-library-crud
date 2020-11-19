b045e711f9aed4650251b73c44d2d5c70d168011
Anderson Santana <anderslash@gmail.com>
2020-11-19 16:24:39 -0300

    Requisito 01 concluido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages/index.js';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD - Anderson Souza Santana</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
