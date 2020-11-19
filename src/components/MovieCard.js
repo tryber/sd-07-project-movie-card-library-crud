beff10fd63a9206912dede5f07fd108f0d585b63
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-19 19:37:20 -0300

    corrigido caminho do MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,18 +4,13 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { id, title, subtitle, storyline, imagePath } = this.props.movie;
+    const { movie } = this.props;
     return (
-      <div className="movie-card" data-testid="movie-card">
-        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
-        <div className="movie-card-body">
-          <h4 data-testid="movie-card-title" className="movie-card-title">
-            {title}
-          </h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-        </div>
-        <Link to={`movies/${id}`}>VER DETALHES</Link>
+      <div data-testid="movie-card">
+        Movie Card
+        <h1>{movie.title}</h1>
+        <p>{movie.storyline}</p>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
