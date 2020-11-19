d0aca47de144025bb7a0b66421b0e6d6d0657f94
Aurelio Scarante <aurelioscarante25@gmail.com>
2020-11-19 19:45:04 -0300

    :white_check_mark: Requisito 3, construindo componente MovieCard :construction_worker:

--- src/App.test.js
+++ src/App.test.js
@@ -141,7 +141,7 @@ describe('2 - Movie list: Ao ser montado, MovieList deve fazer uma requisição 
   })
 });
 
-describe.skip('3 - MovieCard: deve possuir um link para a página de detalhes de um filme', () => {
+describe('3 - MovieCard: deve possuir um link para a página de detalhes de um filme', () => {
   test('deve exibir pelo menos o título e a sinopse de seu respectivo filme', async () => {
     const { unmount, getAllByText } = renderPath('/');
     await waitFor(() => movieAPI.getMovies());
@@ -162,7 +162,7 @@ describe.skip('3 - MovieCard: deve possuir um link para a página de detalhes de
   })
 })
 
-describe.skip('4 - MovieDetails: deve fazer uma requisição para buscar o filme que deverá ser renderizado', () => {
+describe('4 - MovieDetails: deve fazer uma requisição para buscar o filme que deverá ser renderizado', () => {
 
   it('deverá exibir o texto "Carregando..." enquanto estiver fazendo a requisição', () => {
     readMovies().forEach(async (movie) => {
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, title, storyline, imagePhath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{title}</h4>
+        <h4>{storyline}</h4>
+        <Link to={`movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
