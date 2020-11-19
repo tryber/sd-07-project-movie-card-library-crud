0044b91c14a41ec83ce6d9de1c6ea8f5049d2fcc
Strongreen <strongreen@live.com>
2020-11-19 20:34:49 -0300

    Requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
+        <Link className="link-movie-card" to="/movies/:id">VER DETALHES</Link>
       </div>
     );
   }
