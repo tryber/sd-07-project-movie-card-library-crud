f0acd0987ebd8e5316edc037f2f9e346bd0ab03e
Julio Kauer <juliokauer@gmail.com>
2020-11-19 17:41:45 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieDetails, MovieList, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" render={(props) => (<EditMovie {...props} />)} />
+        <Route path="/movies/:id" render={(props) => (<MovieDetails {...props} />)} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </Router>
   );
 }
