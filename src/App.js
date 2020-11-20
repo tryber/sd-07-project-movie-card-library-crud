d13100153643600aff8aa8b4f3ae0ceb23637bd1
Strongreen <strongreen@live.com>
2020-11-20 20:04:24 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
