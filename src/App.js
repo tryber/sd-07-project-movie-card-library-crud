32aa1d52ca74acd856ea77ff66dcff13d26c4828
Israel Menon <zezomenon@gmail.com>
2020-11-20 19:55:47 -0300

    Refatorando primeiro requisito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route path="" component={NotFound} />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
