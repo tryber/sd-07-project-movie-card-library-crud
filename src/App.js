68311a2b9ac4603a020ba31fc545804175056ab6
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-11-20 18:06:47 -0300

    Criado rotas no component App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDatails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter> 
-      <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Router exact path="/" component={MovieList} />
+        <Router path="/movies/:id" component={MovieDatails} />
+        <Router path="/movies/new" component={NewMovie} />
+        <Router path="/movies/:id/edit" component={EditMovie} />
+        <Router component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
