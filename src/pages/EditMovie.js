bf128c75888dbcbac6ceb92f9aab43c0f360b737
moises <moisaant@gmail.com>
2020-11-20 18:52:08 -0300

    correcao PropTypes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -55,6 +55,12 @@ class EditMovie extends Component {
   }
 }
 
-EditMovie.propTypes = { match: PropTypes.arrayOf(PropTypes.object).isRequired };
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
 
 export default EditMovie;
