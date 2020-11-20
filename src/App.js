3b9ca41be2b5d67fcc41202b42419352be83a3be
Carol Bezerra <carol.arbe@gmail.com>
2020-11-20 20:18:38 -0300

    Tentativa 2 do Req.1 e adiantando Req.3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import { MovieDetails, NewMovie, EditMovie, MovieList, NotFound } from './pages/index';
 
-
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/" component={MovieList} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, imagePath } = this.props.movie;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img scr={imagePath} alt={title} />
+        <h3>{title}</h3>
+        <h4>{subtitle}</h4>
+        <p>{storyline}</p>
+        <p><Link to={`movies/${id}`}>VER DETALHES</Link></p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
