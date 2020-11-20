2f394b34534ad30a8464e56e662331fcb8e797df
mbjunqueiraweb <mbjunqueira@gmail.com>
2020-11-20 15:32:32 -0300

    Retomando o projeto apos uma longa parada

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,11 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div data-testid="movie-card">
-        Movie Card
-        <h1>{movie.title}</h1>
-        <p>{movie.storyline}</p>
-        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
+      <div className="movie-card" data-testid="movie-card">
+        <img alt="Movie Cover" className="movie-card-image" src={imagePath} />
+        <div className="movie-card-body">
+          <h4 data-testid="movie-card-title" className="movie-card-title">
+            {title}
+          </h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
