828df0db4031129bc4d6bf46e4d2e2d5b6abe51f
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-11-19 21:00:25 -0300

    organiza de acordo com o CC

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: undefined,
+      title: '',
+      subtitle: '',
+      storyline: '',
+      rating: undefined,
+      imagePath: '',
+      bookmarked: false,
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchDetails();
+  }
+
+  fetchDetails() {
+    const { id } = this.props.match.params;
+    this.setState({ isLoading: true },
+      async () => {
+        const movie = await getMovie(id);
+        this.setState({ ...movie, isLoading: false });
+      });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { title, storyline, imagePath, genre, rating, subtitle, isLoading } = this.state;
+    if (isLoading) {
+      return <Loading />;
+    }
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
-      </div>
-    );
+      <div className="movie-det">
+        <div data-testid="movie-details" className="movie-details">
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <h3>{`Title: ${title}`}</h3>
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>
+        </div>
+      </div>);
   }
 }
 
-export default MovieDetails;
+export default withRouter(MovieDetails);
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(),
+    id: PropTypes.number,
+  }).isRequired,
+};
