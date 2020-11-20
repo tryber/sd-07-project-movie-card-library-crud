f4b1f18e70390cf997e23be02427c8fb4a6446f7
Strongreen <strongreen@live.com>
2020-11-20 19:32:30 -0300

    um item do requisito 6

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -14,11 +14,16 @@ class MovieDetails extends Component {
       loading: false,
     };
     this.getMovie = this.getMovie.bind(this);
+    // this.deletMovie = this.deletMovie.bind(this);
   }
 
   componentDidMount() {
     this.getMovie();
   }
+  // componentWillUnmount() {
+  //   this.deletMovie();
+  // }
+
 
   getMovie() {
     this.setState({ loading: true }, async () => {
@@ -29,6 +34,13 @@ class MovieDetails extends Component {
     });
   }
 
+  // deletMovie(idDelete) {
+  //   this.setState({ loading: true }, async () => {
+  //     const pegamovie = await movieAPI.deleteMovie(idDelete);
+  //     this.setState({ loading: false, movie: aMovie });
+  //   });
+  // }
+
   render() {
     const { loading, movie } = this.state;
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
@@ -37,7 +49,7 @@ class MovieDetails extends Component {
         {loading ? (
           <Loading />
         ) : (
-          <div>
+          <div className="movie-card-details">
             <img alt="Movie Cover" src={`../${imagePath}`} />
             <p>{`${title}`}</p>
             <p>{`Subtitle: ${subtitle}`}</p>
@@ -51,7 +63,9 @@ class MovieDetails extends Component {
               <Link className="link-movie-card" to="/">
                 VOLTAR
               </Link>
-              <button className="link-movie-card" onClick={movieAPI.deleteMovie}>DELETAR</button>
+              <Link to="/">
+                <button className="link-movie-card">DELETAR</button>
+              </Link>
             </div>
           </div>
         )}
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
