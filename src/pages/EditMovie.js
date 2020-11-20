2fd58e6de8dee6eb37a77860cb0e70a35a39802e
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-19 23:35:19 -0300

    Resolvendo os Requisitos e Bonus

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      status: '',
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { id } = this.props.match.params;
+    this.setState({ status: 'loading' }, () => {
+      movieAPI.getMovie(id)
+        .then((movie) => {
+          this.setState({
+            status: '',
+            movie,
+          });
+        });
+    });
   }
 
   handleSubmit(updatedMovie) {
+    this.setState({ status: 'loading' }, () => {
+      movieAPI.updateMovie(updatedMovie)
+        .then(() => {
+          this.setState({
+            status: '',
+            shouldRedirect: true,
+          });
+        });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return (
+        <Redirect to="/" />
+      );
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (
+        <Loading message="Carregando..." />
+      );
     }
 
     return (
@@ -30,5 +66,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { id } = this.props.match.params;
+    this.setState({ loading: true }, () => {
+      movieAPI.getMovie(id)
+        .then((movie) => {
+          this.setState({
+            loading: false,
+            movie,
+          });
+        });
+    });
+  }
+
+  handleClick() {
+    const { movie: { id } } = this.state;
+    movieAPI.deleteMovie(id);
+  }
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { loading, movie } = this.state;
+    if (loading) {
+      return (
+        <Loading message="Carregando..." />
+      );
+    }
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to="/" onClick={this.handleClick}>DELETAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
