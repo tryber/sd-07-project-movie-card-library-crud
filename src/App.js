511d27a8edb1c56e2efa4c9ea5bd9f37544a11ce
cezar88augusto <cezar88augusto@gmail.com>
2020-11-20 19:12:31 -0300

    Projeto Finalizado! :boob:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import './App.css';
+
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Link to="/">Movie List</Link>
+      <Link to="/movies">Movie Details</Link>
+      <Link to="/movies/new">New Movie</Link>
+      <Link to="/edit">Edit</Link>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      <Switch>
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, imagePath, subtitle } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
-      </div>
+        <h4>{title}</h4>
+        <img src={imagePath} alt="movie-folder" />
+        <div>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
+      </div >
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
