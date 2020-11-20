2da34e8ba75dd85ed0503b2bc55141cc24df2586
Lorena Góes <goesloren@gmail.com>
2020-11-19 22:10:52 -0300

    MovieCard: setting Link

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 class MovieCard extends React.Component {
   render() {
-    const { title, storyline, imagePath } = this.props.movie;
+    const { id, title, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
         <h2>{title}</h2>
         <h3>{storyline}</h3>
         <img src={imagePath} alt={`${title} movie`} />
+        <Link to={`movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
@@ -16,6 +18,7 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.string,
     title: PropTypes.string,
     imagePath: PropTypes.string,
     storyline: PropTypes.string,
