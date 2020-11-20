373475fb17bd38ca9233f3ab641f830834908df2
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-20 16:15:57 -0300

    MovieCard: deve possuir um link para a página de detalhes de um filme

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <Switch>
           <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+          <Route exact path="/" render={(props) => <MovieList {...props} />} />
           <Route path="/404" component={NotFound} />
           <Redirect from="*" to="/404" />
         </Switch>
