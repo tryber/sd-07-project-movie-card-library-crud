4e2002ffa86d9bfa9d947d839a1bb2f45dc11c87
Felipe Nascimento <flpnascto@gmail.com>
2020-11-20 17:19:40 -0300

    Requisito 06.

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      shouldRedirect: false,
+    };
   }
 
-  // handleSubmit(newMovie) {
-  // }
+  handleSubmit(newMovie) {
+    this.setState(
+      async () => {
+        await movieAPI.createMovie(newMovie);
+        this.setState({
+          shouldRedirect: true,
+        });
+      },
+    );
+  }
 
   render() {
+    const { shouldRedirect } = this.state;
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        {shouldRedirect ? <Redirect to="/" /> : <MovieForm onSubmit={this.handleSubmit} />}
       </div>
     );
   }
