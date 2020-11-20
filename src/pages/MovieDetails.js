2591bc59efaa7eab284c83c6158b17f8ab509c96
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-20 17:34:09 -0300

    MovieDetails: deve fazer uma requisição para buscar o filme que deverá ser renderizado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieDetails: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovieDetails();
+  }
+
+  async fetchMovieDetails() {
+    const { id } = this.props.match.params;
+    const movieDetails = await movieAPI.getMovie(id);
+    this.setState({
+      movieDetails,
+      loading: false,
+    });
+  }
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { loading } = this.state;
+    if (loading) return <Loading />;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movieDetails;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
@@ -18,9 +39,17 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div>
+          <button><Link to={`/movies/${id}/edit`}>EDITAR</Link></button>
+          <button><Link to="/">VOLTAR</Link></button>
+        </div>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape().isRequired,
+};
 
 export default MovieDetails;
