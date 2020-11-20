39cb5ecd6eb80395c4ecd8d43b015526fb58eecb
moacyrrln <moacyrrln@gmail.com>
2020-11-20 18:51:07 -0300

    1851

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import EditMovie from './pages/EditMovie';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
 import MovieList from './pages/MovieList';
-import { Route, Switch } from 'react-router-dom';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <main>
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/" component={MovieList} />
-      </main>
+      <Switch>
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/" component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
