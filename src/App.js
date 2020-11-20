6880b9cc66e4f1a22ffea7189bfb1e32d7e7f3b0
Carol Bezerra <carol.arbe@gmail.com>
2020-11-20 18:43:27 -0300

    Tentativa Requisito 1 e adiantando Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { MovieDetails, NewMovie, EditMovie, MovieList, NotFound } from './pages/index';
+
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
     this.state = {
       movies: [],
-    }
+      loading: true,
+    };
   }
+
+  componentDidMount() {
+    this.fetchHere();
+  }
+
+  fetchHere() { // para buscar a API, não precisa JSON porque não é uma http
+    movieAPI.getMovies().then((movies) => this.setState({
+      movies,
+      loading: false,
+    }));
+  } // a partir do retorno da API é que se altera o estado
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    // Enquanto a API não retorna para mudar o estado, Loading é executado com a mensagem
+    if (this.state.loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
