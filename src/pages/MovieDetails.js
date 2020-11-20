08bc22ad390041c9465d970500a5cf2e57b498be
Julio Kauer <juliokauer@gmail.com>
2020-11-20 12:48:46 -0300

    Delete

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -10,20 +10,36 @@ class MovieDetails extends Component {
 
     this.state = {
       movie: {},
-      loading: true,
+      status: 'loading',
+      shouldRedirect: false,
     };
+    this.delete = this.delete.bind(this);
   }
 
   componentDidMount() {
     movieAPI.getMovie(this.props.match.params.id)
-    .then((movie) => this.setState({ movie, loading: false }));
+    .then((movie) => this.setState({ movie, status: 'details' }));
+  }
+
+  delete() {
+    const { id } = this.state.movie;
+    movieAPI.deleteMovie(id)
+    .then(() => this.setState({ shouldRedirect: true }));
   }
 
   render() {
-    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    const { movie, status, shouldRedirect } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
+    if (status === 'loading') {
+      return <Loading />;
+    }
 
     return (
-      this.state.loading ? <Loading /> :
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
         <h1>{`Title: ${title}`}</h1>
@@ -33,6 +49,7 @@ class MovieDetails extends Component {
         <p>{`Rating: ${rating}`}</p>
         <Link to="/" >VOLTAR</Link>
         <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <a href="/" onClick={this.delete}>DELETAR</a>
       </div>
     );
   }
