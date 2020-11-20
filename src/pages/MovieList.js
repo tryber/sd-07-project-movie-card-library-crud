2cce7642a5b7fb7fb427a9760e81b92675ca16c5
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-20 19:21:57 -0300

    Initial commit - Project Movie Card Library Crud

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+/* import * as movieAPI from '../services/movieAPI'; */
 
 class MovieList extends Component {
   constructor() {
@@ -9,7 +9,7 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-    }
+    };
   }
 
   render() {
