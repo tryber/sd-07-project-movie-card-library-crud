165f11a21fa96e0ed53dbb3247926c803ec667d9
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-11-21 15:29:41 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
 function App() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id/edit" component={EditMovie} />
-          <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
-          <Route exact path="/" component={MovieList} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
