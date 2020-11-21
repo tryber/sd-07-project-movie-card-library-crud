eb8d1d9f8f02f933aaaa9c6c53e6b31c376fb55b
dennerpl <denner.souzapl@gmail.com>
2020-11-21 16:16:48 -0300

    movie details finished

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <BrowserRouter>
     <Switch>
     <Route exact path="/" component={MovieList} />
-    <Route path="/movies/:id" render={(...props) => <MovieDetails />} />
+    <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+    <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
     <Route path="/movies/new" component={NewMovie} />
-    <Route path="/movies/:id/edit" component={EditMovie} />
     <Route path="*" component={NotFound} />
     </Switch>
     </BrowserRouter>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.findMovie = this.findMovie.bind(this);
+
+    this.state = {
+      movie: {},
+      loading: true,
+    }
+  }
+
+  async findMovie(id) {
+    const movieById = await movieAPI.getMovie(id);   
+    this.setState({
+      movie: movieById,
+      loading: false,
+    })
+  }
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.setState({ id: id});
+    this.findMovie(id);
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    if(this.state.loading) {
+      return <Loading />
+    }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
+      <div>
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
+    <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+      </div>
+      <div>
+      <Link to={"/movies/" + this.state.id + "/edit"}>EDITAR</Link>
+      <Link to={"/"}>VOLTAR</Link>
+      </div>
       </div>
     );
   }
