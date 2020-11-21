3c8577b01cea096983f5527a8da00fe06e97cf9f
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-21 15:18:15 -0300

    requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { storyline, imagePath, genre, rating, subtitle } = this.props;
-    // const { id } = this.props.match.params;
+    this.requireMovie = this.requireMovie.bind(this);
+
+    this.state = {
+      movie: null,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.requireMovie(id);
+  }
+
+  async requireMovie(id) {
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  render() {
+    if (this.state.loading) return <Loading />;
+    const { id } = this.props.match.params;
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h1>{`Title: ${title}`}</h1>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
@@ -27,9 +53,9 @@ class MovieDetails extends Component {
 export default MovieDetails;
 
 MovieDetails.propTypes = {
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  subtitle: PropTypes.string.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
 };
