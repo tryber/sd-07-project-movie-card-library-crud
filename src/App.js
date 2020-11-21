c8ad0ab21eaf34574d041115cb7d757a0151b5f2
Anderson Meira <contatodeanderson@gmail.com>
2020-11-21 20:57:28 -0300

    Rotas app.js - fix

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />  
         <Route component={NotFound} />
       </Switch>
     </Router>
