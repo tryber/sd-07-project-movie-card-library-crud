5274f9dd9649ad3e766dd2902d636b4f49d5724e
Anderson Meira <contatodeanderson@gmail.com>
2020-11-21 20:52:46 -0300

    Rotas app.js - cc fix

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route exact path='/movies/:id' component={MovieDetails} />
-        <Route exact path='/movies/new' component={NewMovie} />
-        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </Router>
