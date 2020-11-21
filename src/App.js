c531754eba161241b3583db81a526ffe93f3210e
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-21 16:40:39 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -10,11 +10,13 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route exact path="/" component={MovieList} />
-      <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
-      <Route path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="*" component={NotFound} />
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route exact path="/movies/:id/edit" render={(props) => <EditMovie {...props} />} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
