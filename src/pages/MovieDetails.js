b1db368ecde6842acfc9fc4075a51cab99b9ac43
renatomak <renato.mark.silva@gmail.com>
2020-11-22 17:55:12 -0300

    correção de erros2-CC-2

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
@@ -57,5 +58,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }),
+};
 
 export default MovieDetails;
