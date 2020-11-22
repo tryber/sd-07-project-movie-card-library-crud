cc52a5d7e4c979c3d101c14a9ddb79604aede491
Anderson Meira <contatodeanderson@gmail.com>
2020-11-22 17:02:01 -0300

    MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      bookmarked,
+      genre,
+      id,
+      imagePath,
+      rating,
+      storyline,
+      subtitle,
+      title
+    } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{title}</p>
+        <p>{storyline}</p>
+        <Link to={`movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
