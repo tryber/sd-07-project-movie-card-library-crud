1ec51fdea60210ae6aaaf392f81d422451f6ec1c
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-22 20:25:46 -0300

    Commit: 0.1.0

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.fetchMoviesId = this.fetchMoviesId.bind(this);
+    this.state = {
+      movie: "",
+      loading: true,
+    }
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.fetchMoviesId(id);
+  }
+
+  async fetchMoviesId(id) {
+    const requestReturn = await movieAPI.getMovie(id);    
+    this.setState({
+      movie: requestReturn,
+      loading: false,
+    })
+  }
+
+  
+  
+  render() {
+                
+    if (this.state.loading) return "Carregando...";
+    
+    const myMovie = this.state.movie;
+  
+    const { title, storyline, imagePath, genre, rating, subtitle } = myMovie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <button type="button">  
+          <Link to="/">VOLTAR</Link>        
+        </button>        
       </div>
     );
   }
--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -17,7 +17,7 @@ export const getMovies = () => (
 
 export const getMovie = (movieId) => {
   const movie = readMovies().find((mov) => mov.id === parseInt(movieId, 10));
-
+  
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(movie);
