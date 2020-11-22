c0f73e6bb2709656410330f1af55389029b0d968
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-22 00:51:59 -0300

    Continuando requisito 4

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={MovieList} />
+          <Route exact path="/" component={MovieList} />
           <Route path="/movies/:id" component={MovieDetails} />
           <Route path="/movies/new" component={NewMovie} />
           <Route path="/movies/:id/edit" component={EditMovie} />
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.renderMovie = this.renderMovie.bind(this);
+    this.state = {
+      isLoading: true,
+      movie: [],
+    };
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  fetchMovie() {
+    const { id } = this.props.match.params;
+    this.setState({ isLoading: true }, async () => {
+      const returnedMovie = await movieAPI.getMovie(id);
+      console.log(returnedMovie);
+      this.setState({
+        isLoading: false,
+        movie: returnedMovie,
+      });
+    });
+  }
 
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  renderMovie() {
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movie;
     return (
-      <div data-testid="movie-details">
+      <div>
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <div>
+          <Link to={`/movies/${id}/edit`} >EDITAR</Link>
+          <Link to={'/'} >VOLTAR</Link>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div data-testid="movie-details">
+        {isLoading ? <Loading /> : this.renderMovie()}
       </div>
     );
   }
