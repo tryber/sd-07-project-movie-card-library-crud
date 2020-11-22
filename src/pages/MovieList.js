12380f867c4f884aad02c69a6f5e80f6f6b9f515
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-22 16:45:24 -0300

    Commit: 0.0.1

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -6,17 +6,30 @@ import * as movieAPI from '../services/movieAPI';
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.fetchMovies = this.fetchMovies.bind(this);
     this.state = {
       movies: [],
+      loading: true
     }
+  }
+
+  async fetchMovies() {
+    const requestReturn = await movieAPI.getMovies();        
+    this.setState(({movies}) => ({
+      movies: [...movies, ...requestReturn],
+      loading: false
+    }))
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
+    if (this.state.loading) return "Carregando...";
+    
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
