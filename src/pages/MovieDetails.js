a9e3dd1a61ae9ec7701a56d036b79db7e9c57141
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-22 20:46:09 -0300

    Commit: 0.1.1

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import * as movieAPI from '../services/movieAPI';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+
 
 class MovieDetails extends Component {
   constructor() {
     super();
     this.fetchMoviesId = this.fetchMoviesId.bind(this);
     this.state = {
-      movie: "",
+      movie: {},
       loading: true,
-    }
+    };
   }
 
   componentDidMount() {
@@ -18,23 +20,21 @@ class MovieDetails extends Component {
   }
 
   async fetchMoviesId(id) {
-    const requestReturn = await movieAPI.getMovie(id);    
+    const requestReturn = await movieAPI.getMovie(id);
     this.setState({
       movie: requestReturn,
       loading: false,
-    })
+    });
   }
-
-  
-  
+    
   render() {
                 
-    if (this.state.loading) return "Carregando...";
+    if (this.state.loading) return 'Carregando...';
     
     const myMovie = this.state.movie;
   
-    const { title, storyline, imagePath, genre, rating, subtitle } = myMovie;
-
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = myMovie;
+  
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
@@ -43,12 +43,25 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
-        <button type="button">  
-          <Link to="/">VOLTAR</Link>        
-        </button>        
+        <button type="button">
+          <Link to="/">VOLTAR</Link>
+        </button>
+        <button type="button">
+          <Link to={`/movies/${id}/edit`}>EDITAR </Link>
+        </button>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  genre: PropTypes.string,
+  rating: PropTypes.number,
+}.isRequired;
 
 export default MovieDetails;
--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -16,8 +16,7 @@ export const getMovies = () => (
 );
 
 export const getMovie = (movieId) => {
-  const movie = readMovies().find((mov) => mov.id === parseInt(movieId, 10));
-  
+  const movie = readMovies().find((mov) => mov.id === parseInt(movieId, 10));  
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(movie);
