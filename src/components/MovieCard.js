c8d7c1533c274dea18e2eb733ba19b3962f90d25
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-22 18:25:05 -0300

    Commit: 0.0.7

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { id, title, storyline } = this.props.movie;
-    const name = `/movies/${id}`;
-        
+    const { id, title, storyline } = this.props.movie;    
+    const name = `/movies/${id}`;        
     return (
       <div data-testid="movie-card">
         <header>{title}</header>
         <p>{storyline}</p> <br />
-        <Link to={{pathname: name}}>VER DETALHES</Link>
+        <Link to={{ pathname: name }}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,    
+}.isRequired
 
 export default MovieCard;
