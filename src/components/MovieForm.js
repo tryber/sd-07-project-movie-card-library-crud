91074cca571c6fd4469dbd8f62c09e34895df7d2
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-11-22 11:35:20 -0300

    Requirement bonus

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -155,3 +155,8 @@ class MovieForm extends React.Component {
 }
 
 export default MovieForm;
+
+MovieForm.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.string).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
