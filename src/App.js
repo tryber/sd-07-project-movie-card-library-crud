d11313e82b5ecf114ab1d252aa77e87df9e34c89
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-22 17:12:16 -0300

    Commit: 0.0.4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />   
+        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
         <Route component={NotFound} />
