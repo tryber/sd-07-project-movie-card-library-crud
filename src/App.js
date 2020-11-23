5c451a89e5d0057a3e9149c6e3e4b39e160a04c6
Camila Valentim <camilaifpb@hotmail.com>
2020-11-23 16:38:47 -0300

    commit 0.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library Crud</div>
+      <Switch>
+       <Route path="/" component={MovieList}/>
+       <Route path="/movies/:id" component={MovieDetails}/>
+       <Route path="/movies/new" component={NewMovie}/>
+       <Route path="/movie/:id/edit" component={EditMovie}/>
+       <Route exact path="/" component={NotFound}/>
+      </Switch>
     </BrowserRouter>
   );
 }
