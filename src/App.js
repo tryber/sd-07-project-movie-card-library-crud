34e84fec7889d18b784491803d8b9395f2c7c56b
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-22 21:21:39 -0300

    Alteraçoes para o primeiro requisito p.3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -9,18 +8,15 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <div>Movie Card Library CRUD</div>
-        <Switch>
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/" component={MovieList} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
