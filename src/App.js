59447e3a4d801079b0831df8dfbc8a892de1f286
Camila Valentim <camilaifpb@hotmail.com>
2020-11-23 18:14:01 -0300

    commit 0.5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
-import NotFound from './pages/NotFound';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movie/:id/edit" component={EditMovie} />
-        <Route exact path="/" component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  );
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/" component={MovieList} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
