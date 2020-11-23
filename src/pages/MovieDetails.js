fe188b325482b69a7d46430afb2d833aa239b2c3
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-23 11:26:07 -0300

    Alteraçoes para o terceiro requisito

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+
 
 class MovieDetails extends Component {
   render() {
@@ -13,10 +15,12 @@ class MovieDetails extends Component {
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/">Voltar à home</Link>
       </div>
     );
   }
