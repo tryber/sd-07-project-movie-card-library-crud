9f383157ede8f43b10580509d67d9c25f5110015
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-23 08:42:39 -0300

    primeira tentativa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <div>
-      Movie Card Library CRUD
-    </div>
+    <BrowserRouter>
+      <Route path="/"><MovieList /></Route>
+      <Route
+        path="movies/:id"
+        render={(props) => <MovieDetails
+          {...props}
+          title="recebe algo aqui"
+        />}
+      />
+      <Route path="movies/new"><NewMovie /></Route>
+      <Route
+        path="/movies/:id/edit"
+        render={(props) =>
+          <EditMovie
+            {...props}
+            movie="editar movie"
+        />}
+      />
+    </BrowserRouter>
   );
 }
 
