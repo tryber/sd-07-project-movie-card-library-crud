342f59e47145205650905f35a945bf10cd15ed16
Lorena Góes <goesloren@gmail.com>
2020-11-23 09:42:09 -0300

    Editing MovieList and NewMovie: New Link at Home page

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -28,8 +29,10 @@ class MovieList extends Component {
     const { movies, loading } = this.state;
 
     return (
-      loading ? <Loading /> : <div data-testid="movie-list">
+      loading ? <Loading /> :
+      <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
