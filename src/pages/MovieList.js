a721d4540c052686455865e00c014d21cc41950e
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-23 10:25:26 -0300

    Alteraçoes para o segundo requisito

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.fetchMovies = this.fetchMovies.bind(this);
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    this.setState({
+      loading: true,
+    },
+    async () => {
+      const renderMovies = await movieAPI.getMovies();
+      this.setState({
+        loading: false,
+        movies: renderMovies,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    const loadingElement = <Loading />;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        {loading ? loadingElement : movies
+        .map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
   }
