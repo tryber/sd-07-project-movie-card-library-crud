e34551f29bfe99e974fe74bb7468f184ea3a4883
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-23 12:14:12 -0300

    add items no movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={imagePath} />
+        <h4 data-testid="movie-card-title">
+          {title}
+        </h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
