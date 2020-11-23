bbb179efa15b3b2b39357b6a97c938130ad52f27
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-23 12:22:07 -0300

    add items no movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
@@ -6,7 +7,7 @@ class MovieCard extends React.Component {
     const { id, title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
-        <img src={imagePath} />
+        <img src={imagePath} alt="movie" />
         <h4 data-testid="movie-card-title">
           {title}
         </h4>
@@ -17,5 +18,15 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.PropTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
