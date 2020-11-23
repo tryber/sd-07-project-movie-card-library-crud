12b95bcd24547eb10db60e45696393522ccefb6b
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-23 14:36:46 -0300

    corrigindo espaços no cc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
-import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
+import NewMovie from './pages/NewMovie'
 import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <BrowserRouter>
+class App extends React.Component {
+  render () {
+    return (
       <Switch>
-        <Route exact path="/movies/:id/edit" component={EditMovie} />
-        <Route exact path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
-        <Route exact path="*" component={NotFound} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
-  );
+    );
+  }
 }
+  
 
 export default App;
