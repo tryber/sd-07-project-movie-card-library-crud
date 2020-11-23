52de4505737114366b62a50f46c5225f62b68328
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-23 12:48:08 -0300

    Alteraçoes para o quarto requisito

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.fetchMovies = this.fetchMovies.bind(this);
+    this.state = {
+      loading: true,
+      movie: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    this.setState({
+      loading: true,
+    },
+    async () => {
+      const { id } = this.props.match.params;
+      const renderMovies = await movieAPI.getMovie(id);
+      this.setState({
+        loading: false,
+        movie: renderMovies,
+      });
+    });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { id } = this.props.match.params;
+    const loadingElement = <Loading />;
 
     return (
+      loading ? loadingElement :
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
         <p>{`Title: ${title}`}</p>
@@ -20,10 +52,19 @@ class MovieDetails extends Component {
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
-        <Link to="/">Voltar à home</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
