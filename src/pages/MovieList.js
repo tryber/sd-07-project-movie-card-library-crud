134b00ed668c29b89fe9c996468dd015677ab02c
josielvs <josielvs@gmail.com>
2020-11-24 11:31:38 -0300

    Send requisite 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { MovieCard, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
@@ -36,6 +37,7 @@ class MovieList extends Component {
           loadingRender :
           movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)
         }
+        <Link to="movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import MovieForm from '../components/MovieForm';
+import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      shouldRedirect: false,
+    };
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    const reqNewMovie = await movieAPI.createMovie(newMovie);
+    if (reqNewMovie === 'OK') {
+      this.setState({ shouldRedirect: true });
+    }
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
