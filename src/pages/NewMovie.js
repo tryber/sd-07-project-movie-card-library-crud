6b8c585f7625417f1a62cb22967f984cee0add93
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-24 15:12:38 -0300

    newmovie

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-
-import MovieForm from '../components/MovieForm';
+import { Redirect } from 'react-router-dom';
+import MovieForm from '../components/MovieForm.js';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
+    if (this.state.shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={this.handleSubmit} />
@@ -20,4 +27,5 @@ class NewMovie extends Component {
     );
   }
 }
+
 export default NewMovie;
