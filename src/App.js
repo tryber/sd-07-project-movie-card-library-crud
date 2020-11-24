1c979a9eb8ba4e24ea07d45d814a94b7832c6408
gustasaideles <gustasaideles.gmail.com>
2020-11-24 10:05:13 -0300

    App.js Corrigindo path edit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route exact path="/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </Router>
