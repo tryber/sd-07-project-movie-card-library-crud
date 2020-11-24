35b73bbe16f4cd3a713d9440707131bfc9c070bf
Camila Valentim <camilaifpb@hotmail.com>
2020-11-24 10:58:26 -0300

    cc

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -8,19 +8,15 @@ class MovieDetails extends Component {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.movie;
+    const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
-        <div>
-          <p>{`Title: ${title}`}</p>
-          <p>{`Subtitle: ${subtitle}`}</p>
-          <p>{`Storyline: ${storyline}`}</p>
-          <p>{`Genre: ${genre}`}</p>
-          <p>{`Rating: ${rating}`}</p>
-        </div>
-        <Link to="/" onClick={() => movieAPI.deleteMovie(id)}>DELETAR</Link>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
       </div>
     );
   }
