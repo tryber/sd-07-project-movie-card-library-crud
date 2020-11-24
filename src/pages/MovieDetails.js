a929e1efcc0ecbe362b55af75a3694fe092c377b
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-24 01:03:12 -0300

    add Link para pagina inicial

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -41,6 +41,7 @@ class MovieDetails extends Component {
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
         <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     ) : (
       <Loading />
