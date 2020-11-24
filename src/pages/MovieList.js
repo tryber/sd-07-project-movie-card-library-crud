ae787e289f4729c85029adfc44d3e42e4d119f44
sttefanieMaciel <macielsttefanie@gmail.com>
2020-11-24 14:31:10 -0300

    corrigindo MOVIELIST

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,45 +1,58 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { MovieCard, Loading } from '../components';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading.js'
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
-      isFetching: true,
-    }
+      loading: true,
+    };
   }
 
   componentDidMount() {
-    this.fetchAPI();
+    this.renderMovies();
   }
 
-  async fetchAPI() {
-    const movieList = await movieAPI.getMovies();
-    this.setState({
-      movies: movieList,
-      isFetching: false,
-    })
+  async renderMovies() {
+    const result = await movieAPI.getMovies();
+    this.setState({ movies: result, loading: false });
   }
+
+   // renderMovies() {
+  //   this.setState({ loading: true }, async () => {
+  //     const result = await movieAPI.getMovies();
+  //     this.setState({ movies: result, loading: false });
+  //   });
+  // }
+
 
   render() {
-    const { movies, isFetching } = this.state;
-
-    if (isFetching) return <Loading />;
-
-    console.log(this.state.movies);
-
+    const { movies, loading } = this.state;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        <header className="movie-card-header">
+          <h1 className="page-title">Movie Card Library</h1>
+        </header>
+        <div className="grid">
+          <div className="item">
+            {loading ?
+            <Loading /> : movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+            <div className="div-area-input">
+              <button>
+                <Link className="red" to="/movies/new">ADICIONAR CARTÃO</Link>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
+
   }
-}
+}  
 
 export default MovieList;
