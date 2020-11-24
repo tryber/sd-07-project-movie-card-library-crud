0b5468e9ec93d8d0bde4cc5d2a87045623c22732
Camila Valentim <camilaifpb@hotmail.com>
2020-11-23 23:21:10 -0300

    commit 09

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { id, title, subtitle, storeline, imagePath } = this.props.movie;
+    const { id, title, subtitle, storyline, imagePath } = this.props.movie;
     return (
       <div data-testid="movie-card">
         <img alt="movie" src={imagePath} />
         <div>
           <h1>{title}</h1>
           <h2>{subtitle}</h2>
-          <p>{storeline}</p>
+          <p>{storyline}</p>
         </div>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
@@ -24,7 +24,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
-    storeline: PropTypes.string,
+    storyline: PropTypes.string,
     imagePath: PropTypes.string,
     id: PropTypes.number,
   }).isRequired,
