4d409f9da50e360ec85b5f332487d52db4d459d7
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-11-24 21:19:22 -0300

    Aplicado estilos de card details, e começando a criar o component MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,19 +4,52 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      isLoading: true,
+    };
+  }
+
+  attState(movie) {
+    this.setState({
+      movie,
+      isLoading: false,
+    });
+  }
+
+  async componentDidMount() {
+    const { params } = this.props.match;
+    const fetchPromise = await movieAPI.getMovie(params.id);
+    this.attState(fetchPromise);
+  }
+
+  movieCardDetail() {
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+
+    return (
+      <div className="movie-detail" data-testid="movie-details">
+        <img className="movie-detail-image" alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{title}</p>
+        <div className="movie-detail-body">
+          <p className="movie-card-subtitle">{`Subtitle: ${subtitle}`}</p>
+          <p className="movie-card-storyline">{`Storyline: ${storyline}`}</p>
+          <p className="movie-card-genre">{`Genre: ${genre}`}</p>
+          <p className="rating">{`Rating: ${rating}`}</p>
+        </div>
+      </div>)
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+      <div>
+        {this.state.isLoading === true ? <Loading /> : this.movieCardDetail()}
       </div>
     );
   }
