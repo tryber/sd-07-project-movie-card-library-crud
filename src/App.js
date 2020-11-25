028555f6cb16a76c20b5c749f06f0a7ad207b229
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-25 05:18:17 -0300

    do zero ao topo: requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD - Commit Inicial</div>
-  );
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+ 
 }
 
 export default App;
