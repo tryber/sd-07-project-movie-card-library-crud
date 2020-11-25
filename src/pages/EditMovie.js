6369fb5decbb7862edbd8f5ba72d860435f3834e
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-25 00:13:21 -0300

    Alteraçoes para o quinto requisito

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovies = this.fetchMovies.bind(this);
+    this.state = {
+      loading: true,
+      shouldRedirect: false,
+      movie: [],
+    };
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.fetchMovies(id);
+  }
+
+  async fetchMovies() {
+    this.setState({ loading: true,
+    },
+    async () => {
+      const { id } = this.props.match.params;
+      const renderMovie = await movieAPI.getMovie(id);
+      this.setState({
+        loading: false,
+        movie: renderMovie,
+      });
+    });
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
+    if (loading === true) {
       // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +60,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -62,7 +62,7 @@ class MovieDetails extends Component {
 MovieDetails.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      id: PropTypes.number,
+      id: PropTypes.string.isRequired,
     }),
   }).isRequired,
 };
