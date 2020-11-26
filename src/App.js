0320c50e357f95d5d513edc57bfd11048e3f957d
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-26 18:02:48 -0300

    do zero ao topo: requisito 3 cc issues

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={MovieList} />
           <Route exact path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id" 
+          <Route 
+            exact 
+            path="/movies/:id"
             render={(props) => <MovieDetails {...props} />}
           />
-          <Route exact path="/movies/:id/edit" 
+          <Route 
+            exact 
+            path="/movies/:id/edit"
             render={(props) => <EditMovie {...props} />}
           />
           <Route component={NotFound} />
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -8,12 +9,30 @@ class MovieCard extends React.Component {
     return (
       <div data-testid="movie-card">
         <p>{title}</p>
-        <img src={imagePath}/>
+        <img src={imagePath} alt="" />
         <p>{storyline}</p>
         <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }),
+}
+
+MovieCard.defaultProps = {
+  movie: PropTypes.shape({
+    id: 0,
+    title: '',
+    storyline: '',
+    imagePath: '',
+  }),
 }
 
 export default MovieCard;
