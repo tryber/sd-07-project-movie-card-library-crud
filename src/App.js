b05a3d5578c3c6cdaeeda03f28400ccff32c10c4
Lucas Ribeiro <martins7ribeiro@gmail.com>
2020-11-26 15:41:13 -0300

    do zero ao topo: requisito 1

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={MovieList} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
