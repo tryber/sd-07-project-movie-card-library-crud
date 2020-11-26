69925f464672c9f36eacb90bdd759f87d98033af
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-26 02:28:43 -0300

    Commit: 0.3.5

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,11 +8,11 @@ class MovieCard extends React.Component {
     const name = `/movies/${id}`;
     return (
       <div data-testid="movie-card" className="movie-card">
-        <header className='title-card'>{title}</header>
+        <header className="title-card">{title}</header>
         <img src={imagePath} alt={title} className="img-card" />
-        <p className='text-card'>{storyline}</p> <br />
-        <hr/>
-        <Link to={{ pathname: name }} className='text-link'>VER DETALHES</Link>
+        <p className="text-card">{storyline}</p> <br />
+        <hr />
+        <Link to={{ pathname: name }} className="text-link">VER DETALHES</Link>
       </div>
     );
   }
