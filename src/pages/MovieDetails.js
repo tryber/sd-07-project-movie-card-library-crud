9c0071f8d53b0d1f93577dc29bb5ea71af740f56
spawnred <bruno.spawnhell@gmail.com>
2020-11-26 04:12:47 -0300

    item 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -38,6 +38,9 @@ class MovieDetails extends Component {
           <p>{`Genre: ${genre}`}</p>
           <p>{`Rating: ${rating}`}</p>
           <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link to="/" onClick={() => movieAPI.deleteMovie(id)}>
+             DELETAR
+          </Link>
         </div>
         }
         <Link to="">VOLTAR</Link>
