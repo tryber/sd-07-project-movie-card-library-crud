b138d13540a5ce442b762342feada0c9dac00cc4
gustasaideles <gustasaideles.gmail.com>
2020-11-26 11:26:15 -0300

    ta rodando mas nao passando no 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import PropTypes from 'prop-types';
 
 class MovieDetails extends Component {
   constructor() {
@@ -24,21 +25,32 @@ class MovieDetails extends Component {
   }
 
   render() {
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-    const { loaded } = this.state;
-
+    const { loaded, resultDetails } = this.state;
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = resultDetails;
     if (loaded) {
       return (
         <div data-testid="movie-details">
+          <p>{`Title: ${title}`}</p>
           <img alt="Movie Cover" src={`../${imagePath}`} />
           <p>{`Subtitle: ${subtitle}`}</p>
           <p>{`Storyline: ${storyline}`}</p>
           <p>{`Genre: ${genre}`}</p>
           <p>{`Rating: ${rating}`}</p>
+          <Link to="/">VOLTAR</Link>
+          <br/>
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
         </div>
       );
     } return <Loading />;
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
