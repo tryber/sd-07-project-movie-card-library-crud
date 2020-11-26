6b10dc4ab42c431e409e5e561bf4d3035eaf30fe
spawnred <bruno.spawnhell@gmail.com>
2020-11-26 01:09:43 -0300

    item 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    this.fectMovie();
+  }
+
+  fectMovie() {
+    this.setState({ loading: true }, async () => {
+      const { match } = this.props;
+      const requestMovie = await movieAPI.getMovie(match.params.id);
+      this.setState({ movie: requestMovie, loading: false });
+    });
+  }
+  render() {
+    const { loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+        { loading ? <Loading /> :
+        <div>
+          <img alt="Movie Cover" src={`../${imagePath}`} />
+          <p>{`Title: ${title}`}</p>
+          <p>{`Subtitle: ${subtitle}`}</p>
+          <p>{`Storyline: ${storyline}`}</p>
+          <p>{`Genre: ${genre}`}</p>
+          <p>{`Rating: ${rating}`}</p>
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        </div>
+        };
+            <Link to="">VOLTAR</Link>
       </div>
     );
   }
