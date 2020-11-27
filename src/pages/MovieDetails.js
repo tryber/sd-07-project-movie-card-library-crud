0e3080af0b754ba90742e5d56423ca62cec79bd3
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-26 23:10:34 -0300

    implementing moviedetails page

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,21 +2,49 @@ import React, { Component } from 'react';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  constructor() {
+    super();
+    this.fetch = this.fetch.bind(this);
+
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  async fetch() {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+    if (this.state.loading) return <Loading />;
+
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <img alt="Movie Cover" src={imagePath} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to={"/"}>VOLTAR</Link>
       </div>
     );
   }
