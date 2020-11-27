80ada0091ad2e92d90afcdb8bf46ea48683acba2
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-26 21:10:15 -0300

    implementing movieList.js page with loading component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={MovieList} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Route exact path="/" component={MovieList} />
+      <Route exact path="/movies/:id" component={MovieDetails} />
+      <Route exact path="/movies/new" component={NewMovie} />
+      <Route exact path="/movies/:id/edit" component={EditMovie} />
       <Route component={NotFound} />
     </BrowserRouter>
   );
