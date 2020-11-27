31e9bf81b7e9a33a2e03b97da4baddd40b06a721
Nicholas Torres <nicolas_br11@hotmail.com>
2020-11-27 10:16:06 -0300

    Adicionado detalhes e estilo do mesmo, com links para voltar á home e link de edição de dados do card movie

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -29,16 +29,20 @@ class MovieDetails extends Component {
 
   movieCardDetail() {
     const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
-
+    const { id } = this.props.match.params;
     return (
       <div className="movie-detail" data-testid="movie-details">
         <img className="movie-detail-image" alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{title}</p>
+        <p className="movie-detail-title">{title}</p>
         <div className="movie-detail-body">
-          <p className="movie-card-subtitle">{`Subtitle: ${subtitle}`}</p>
-          <p className="movie-card-storyline">{`Storyline: ${storyline}`}</p>
-          <p className="movie-card-genre">{`Genre: ${genre}`}</p>
+          <p className="movie-detail-subtitle">{`Subtitle: ${subtitle}`}</p>
+          <p className="movie-detail-storyline">{`Storyline: ${storyline}`}</p>
+          <p className="movie-detail-genre">{`Genre: ${genre}`}</p>
           <p className="rating">{`Rating: ${rating}`}</p>
+        </div>
+        <div className="movie-detail-link">
+          <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+          <Link to={`/`}>VOLTAR</Link>
         </div>
       </div>)
   }
