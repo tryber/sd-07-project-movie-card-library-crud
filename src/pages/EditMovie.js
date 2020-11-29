768d9b18cd191fe956cfe266b5e4bfa4c2eee65f
Anderson Meira <contatodeanderson@gmail.com>
2020-11-29 17:04:40 -0300

    EditMovie - cc fix

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -27,24 +27,24 @@ class EditMovie extends Component {
     this.setState({
       movie: response,
       status: '',
-    })
+    });
   }
 
   handleSubmit(updatedMovie) {
     movieAPI.updateMovie(updatedMovie);
     this.setState({
       shouldRedirect: true,
-    })
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      return <Redirect to='/' />
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-     return <Loading />
+      return <Loading />;
     }
 
     return (
