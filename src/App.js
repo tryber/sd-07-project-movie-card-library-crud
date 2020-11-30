099b83bfccef09634d2adeb7fb694302efc756ae
murilopereira006 <muriloedu006@gmail.com>
2020-11-30 16:36:57 -0300

    routes concluidas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieList from './pages/index';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie.js';
+import MovieDetails from './pages/MovieDetails.js';
+import NewMovie from './pages/NewMovie.js';
+import NotFound from './pages/NotFound.js';
 import './App.css';
 
 function App() {
@@ -8,6 +12,11 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/NotFound" component={NotFound} />
+        <Redirect to="/NotFound" />        
       </Switch>
     </BrowserRouter>
   );
