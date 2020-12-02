61690a10a20912ce0fa592407c863b2b0a125e1f
Bruno Dantas <bruno.wesley77@gmail.com>
2020-12-02 16:57:33 -0300

    feat: render NotFound page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import MovieList from './pages/MovieList.js';
+import EditMovie from './pages/EditMovie.js';
 import MovieDetails from './pages/MovieDetails.js';
 import NewMovie from './pages/NewMovie.js';
-import EditMovie from './pages/EditMovie.js';
+import NotFound from './pages/NotFound.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/NotFound" component={NotFound} />
+        <Redirect to="/NotFound" />
       </Switch>
     </BrowserRouter>
   );
