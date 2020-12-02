89ac87fab046f01a98bf80aceaabcf221f4cf1e5
Bruno Dantas <bruno.wesley77@gmail.com>
2020-12-02 16:21:49 -0300

    request 2: render MovieDetails page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MovieList from './pages/MovieList.js';
+import MovieDetails from './pages/MovieDetails.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
       </Switch>
-    </BrowserRouter>  
+    </BrowserRouter>
   );
 }
 
