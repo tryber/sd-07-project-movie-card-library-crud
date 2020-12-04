72b342d3a0eea989b992a72dfa56635149950489
Lets <letsferreiratx@gmail.com>
2020-12-04 18:05:41 -0300

    criando pagina de detalhes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const id = movie.id
     //console.log(movie);
     return (
       <div data-testid="movie-card">
@@ -12,7 +13,7 @@ class MovieCard extends React.Component {
         <p>
           {movie.storyline}
         </p>
-        <Link to={{ pathname: `/movies/${movie.id}`}}>
+        <Link to={{ pathname: `/movies/${id}`, id, }}>
           VER DETALHES
         </Link>
       </div>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 
+import * as movieAPI from '../services/movieAPI';
+
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movie: [],
+    }
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    this.setState(
+      async () => {
+        const { location } = this.props;
+        const id = location.pathname.slice(-1);
+        const response = await movieAPI.getMovie(id);
+        this.setState({
+          loading: false,
+          movie: response,
+        });
+      },
+    );
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    if (loading) {
+      return (<Loading />);
+    } else {
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={{ pathname: '/', }}>
+        VOLTAR
+        </Link>
+        <Link to={{ pathname: `/movies/${id}/edit`, id, }}>
+        EDITAR
+        </Link>
       </div>
     );
+    }
   }
 }
 
