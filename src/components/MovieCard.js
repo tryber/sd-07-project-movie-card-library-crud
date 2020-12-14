2120d4b97d0538bb7f679d4453e8d66b599cd5e3
paulohp7 <ph.pereira.7@gmail.com>
2020-12-14 20:45:30 -0300

    Third Objective try:1

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>Title: {movie.title}</p>
+        <p>Storyline: {movie.storyline}</p>
+        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default MovieCard;
