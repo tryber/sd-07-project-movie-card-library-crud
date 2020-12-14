a01c330f751bb04038665cb21687c18b15da7494
paulohp7 <ph.pereira.7@gmail.com>
2020-12-14 15:29:46 -0300

    Second Objective try:1

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    this.fetchMovie = this.fetchMovie.bind(this);
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.fetchMovie();
+  }
 
-    // Render Loading here if the request is still happening
+  async fetchMovie() {
+    const fetchMovies = await movieAPI.getMovies();
+    this.setState({ movies: fetchMovies, loading: false });
+  }
 
+  movieCards(movies) {
     return (
       <div data-testid="movie-list">
         <p>This is Movie List</p>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
+    );
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+    // Render Loading here if the request is still happening
+    return (
+      loading ? <Loading /> : this.movieCards(movies)
     );
   }
 }
