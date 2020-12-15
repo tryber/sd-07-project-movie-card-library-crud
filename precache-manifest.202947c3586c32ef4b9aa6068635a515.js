522c205c3fa2ab975528d8442f31002cddf7ffcc
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-15 01:28:21 -0300

    Updates

---
+++ precache-manifest.202947c3586c32ef4b9aa6068635a515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9986a53061670bf3817b6811852003f3",
+    "url": "/trybe-react-project-movie-card-library-crud/index.html"
+  },
+  {
+    "revision": "4f552eaea2dfdb8d852d",
+    "url": "/trybe-react-project-movie-card-library-crud/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cbf4537a8e18d97fb447",
+    "url": "/trybe-react-project-movie-card-library-crud/static/js/2.61ebad88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trybe-react-project-movie-card-library-crud/static/js/2.61ebad88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f552eaea2dfdb8d852d",
+    "url": "/trybe-react-project-movie-card-library-crud/static/js/main.3e0f7bed.chunk.js"
+  },
+  {
+    "revision": "2111af5ac149f09eef8b",
+    "url": "/trybe-react-project-movie-card-library-crud/static/js/runtime-main.0337d94b.js"
+  }
+]);
