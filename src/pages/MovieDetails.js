6b393b10409a9bef6446f53868503f73151a67b2
Carol Bezerra <carol.arbe@gmail.com>
2020-12-18 17:38:51 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,50 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
 
-// class MovieDetails extends Component {
-//   render() {
-//     // Change the condition to check the state
-//     // if (true) return <Loading />;
+  componentDidMount() {
+    // tem que buscar o caminho do id
+    movieAPI.getMovie(this.props.match.params.id).then((movie) => this.setState({
+      movie,
+      loading: false,
+    }));
+  }
 
-//     const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  render() {
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    if (this.state.loading) return <Loading />;
+    return (
+      <div data-testid="movie-details">
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h5>{`Title: ${title}`}</h5>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+      </div>
+    );
+  }
+}
 
-//     return (
-//       <div data-testid="movie-details">
-//         <img alt="Movie Cover" src={`../${imagePath}`} />
-//         <p>{`Subtitle: ${subtitle}`}</p>
-//         <p>{`Storyline: ${storyline}`}</p>
-//         <p>{`Genre: ${genre}`}</p>
-//         <p>{`Rating: ${rating}`}</p>
-//       </div>
-//     );
-//   }
-// }
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
-// export default MovieDetails;
+export default MovieDetails;
