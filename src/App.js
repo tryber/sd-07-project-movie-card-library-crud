e07e676f4d277186b8fe4acfec4364e43c9e2be8
Carol Bezerra <carol.arbe@gmail.com>
2020-12-18 15:35:34 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
-        <Route component={() => <NotFound />} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
     };
   }
 
+componentDidMount() {
+  // faz a requisição (API local não precisa do JSON) e muda o valor do estado
+  movieAPI.getMovies().then((movies) => this.setState({
+    movies,
+  }))
+}
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    if (movies.length === 0) return <Loading />;
+    // se o movies.length === 0 Loading else movies.map
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
