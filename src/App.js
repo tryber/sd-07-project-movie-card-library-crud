5754f6d173987c8c5df1f918e8ba3d2a8a2831da
ErlingSJunior <erlingjunior@gmail.com>
2020-12-20 19:09:01 -0300

    Rotas Ajustadas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/EditMovie';
 
 
@@ -11,13 +11,15 @@ import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
-      <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route Path="/movies/new" component={NewMovie} />
-      <Route path="movies/:id/edit" component={EditMovie} />
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact Path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="movies/:id/edit" component={EditMovie} />
+        <Route component={() => <h1 data-testid="404-error"></h1>}>NotFound</Route>
+      </Switch>
+    </Router>
   );
 }
 
