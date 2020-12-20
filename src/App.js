8623724e2147f4ad297c8cc757327cc8a6471a11
ErlingSJunior <erlingjunior@gmail.com>
2020-12-20 18:55:46 -0300

    Rotas Criadas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { browseRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+
+
+
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route Path="/movies/new" component={NewMovie} />
+      <Route path="movies/:id/edit" component={EditMovie} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+
+
 
 class MovieList extends Component {
   constructor() {
