5e927d915efd86ab0b47d3a9662b9c760e137996
Fabio Camargos <fabiocmmcm@gmail.com>
2020-12-23 17:53:20 -0300

    movieL fixing

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
-import { Link } from 'react-router-dom';
+
 
 class MovieList extends Component {
   constructor() {
@@ -14,10 +15,10 @@ class MovieList extends Component {
       loading: true,
     }
   }
- 
+
   componentDidMount() {
     movieAPI.getMovies()
-    .then((response) => this.setState({movies: response, loading: false}));
+    .then((response) => this.setState({ movies: response, loading: false }));
   }
   
   render() {
@@ -25,12 +26,14 @@ class MovieList extends Component {
     if (loading === true) { return <Loading />; }
 
     return (
-      <div>
+      <section>
+        <Link to="movies/new">ADICIONAR CARTÃO</Link>
         <div data-testid="movie-list">
           {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
         </div>
-      </div>
+      </section>
     );
   }
 }
+
 export default MovieList;
