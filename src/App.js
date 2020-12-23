060257e2455753e6b2e4ccd086fa24c14e3eb3b1
Fabio Camargos <fabiocmmcm@gmail.com>
2020-12-23 16:18:08 -0300

    Add Loading carregando

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -10,13 +10,13 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <switch>
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route path="/" component={MovieList} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
-      </switch>
+      </Switch>
     </BrowserRouter>
   );
 }
