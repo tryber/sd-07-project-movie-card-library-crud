9c9f5b62e7bc862e61226d0274500a2357660401
Fabio Camargos <fabiocmmcm@gmail.com>
2020-12-23 17:39:43 -0300

    movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { id, title, subtitle, storyline, imagePath } = this.props.movie
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <img src={imagePath} alt={`Capa do filme ${title}`}></img>
+        <h5>{storyline}</h5>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
-
 export default MovieCard;
