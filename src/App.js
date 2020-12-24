614868f51e8f8c466b6b6c718d2628d65de3daaf
Arthur de Sa <arthurdesa94@gmail.com>
2020-12-24 17:48:23 -0300

    voltando a ativa, simbora

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
+    <Router>
     <div>Movie Card Library CRUD</div>
+  </Router>
   );
 }
 
