1380cc9519853fdeff3bb9778b656cea08d3f1e3
Andre <dehhmenezes87@gmail.com>
2020-12-25 16:42:56 -0300

    MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,14 +10,26 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchMovieList();
+  }
+
+  async fetchMovieList() {
+    this.setState({
+      movies: await movieAPI.getMovies(),
+      loading: false,
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-    console.log(movieAPI);
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
