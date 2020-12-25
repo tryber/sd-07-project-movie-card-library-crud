2d03af5b9a9e71ef49fcc7542954d486012037db
Arthur de Sa <arthurdesa94@gmail.com>
2020-12-24 21:04:29 -0300

    adding all files that I was fixing in my local

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,18 +10,25 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     }
   }
 
+  componentDidMount() {
+    movieAPI.getMovies()
+    .then((response) => this.setState({ movies: response, loading: false }));
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { loading, movies } = this.state;
+    if (loading === true) { return <Loading />; }
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
+      <section>
+        <Link to="movies/new">ADICIONAR CARTÃO</Link>
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
+      </section>
     );
   }
 }
