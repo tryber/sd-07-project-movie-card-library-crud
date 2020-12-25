db1308f84cf266bdab19e434f48ce31a9e82f671
Andre <dehhmenezes87@gmail.com>
2020-12-25 17:11:12 -0300

    MovieCard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
-        <Route component={NotFound} />
+        <Switch>
+          <Route path="/movies/new" component={NewMovie} />
+          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+          <Route exact path="/" component={MovieList} />
+          <Route component={NotFound} />
+        </Switch>
       </BrowserRouter>
     );
   }
