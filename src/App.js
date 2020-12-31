5df9ad64788465df205a1ac8afebca143c6507ff
ErlingSJunior <erlingjunior@gmail.com>
2020-12-31 13:18:38 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={MovieList} />
-      <Route exact Path="/movies/new" component={NewMovie} />
-      <Route exact path="/movies/:id" component={MovieDetails} />
-      <Route exact path="movies/:id/edit" component={EditMovie} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/" component={MovieList} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { MovieCard } from '../components/MovieCard';
+import { Loading } from '../components';
+import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
+import movies from '../services/movieData';
+
+
 
 class MovieList extends Component {
   constructor() {
@@ -11,17 +15,32 @@ class MovieList extends Component {
     }
   }
 
-  render() {
-    const { movies } = this.state;
+ componentDidMount() {
+   movieAPI.getMovies().then((movies) => (this.setState({movies})))
+ }
 
-    // Render Loading here if the request is still happening
 
-    return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+render() {
+  const { movies } = this.state;
+
+  // Render Loading here if the request is still happening
+  if (movies.length === 0) {
+    return <Loading />
   }
+  return (
+    <div data-testid="movie-list">
+      {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+    </div>
+  );
 }
+
+}
+
+
+
+
+
+
+
 
 export default MovieList;
