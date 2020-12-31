f1aaabe1c8cb33e8cb1c6fb8552d69b5e74f6101
ErlingSJunior <erlingjunior@gmail.com>
2020-12-31 13:26:38 -0300

    ajustando climate 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -23,10 +23,10 @@ class MovieList extends Component {
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
     if (movies.length === 0) {
       return <Loading />
     }
+
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
