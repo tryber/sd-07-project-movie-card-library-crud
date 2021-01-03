6982fbaf83d456145f8d5fd1c50be0cec359924c
ErlingSJunior <erlingjunior@gmail.com>
2021-01-03 13:24:27 -0300

    hora de testar o testador :rocket:

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
-class MovieDetails extends Component {
+
+class MovieDetails extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      movie: [],
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovie(this.props.match.params.id).then((movie) => this.setState({ movie }))
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movie } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (movie.length === 0) {
+      return <Loading />
+    }
+
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to="/">VOLTAR</Link>
+        <br />
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -5,8 +5,7 @@ import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
-    super();
-
+    super()
     this.state = {
       movies: [],
     };
