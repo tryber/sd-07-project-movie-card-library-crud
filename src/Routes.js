c6659012fed96a8d7ee3c7e5af4d395d86255877
MarceloBorges <melombm@hotmail.com>
2021-01-05 14:35:18 -0500

    correçao  add um class no arquivo routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
-const Routes = () => {
-  return (
-    <Switch>
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={NewMovie} />
-        <Route path="/movies/new" component={EditMovie} />
-        <Route path="/movies/:id/edit" component={NotFound} />
-    </Switch>
-  );
-};
+        <Route exact path="" component={NotFound} />
+      </Switch>
+    );
+  }
+}
 
 export default Routes;
