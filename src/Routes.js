89a1e8e6703635d8401e75b26d59db009fc421c7
MarceloBorges <melombm@hotmail.com>
2021-01-06 16:30:34 -0500

    correçao codecliamte

--- src/Routes.js
+++ src/Routes.js
@@ -7,10 +7,10 @@ class Routes extends React.Component {
     return (
       <Switch>
         <Route exact path="/" component={MovieList} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="*" component={NotFound} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="*" component={NotFound} />
       </Switch>
     );
   }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
-
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import {Link} from 'react-router-dom';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovei();
+  }
+
+  async fetchMovei() {
+    const { id } = this.props.match.params; 
+    const endpoint = await movieAPI.getMovie(id);
+    this.setState({
+      movie: endpoint,
+      loading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { loading }  = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (loading) {
+      return <Loading />;
+    }
+
+    const { title, id, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
+        
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h3>{`Title: ${title}`}</h3>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
         <p>movei details</p>
+        <Link to={"/"}>VOLTAR</Link>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
       </div>
     );
   }
