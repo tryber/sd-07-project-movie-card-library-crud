5e108a788cb4e898964f575072dad78efa3dea79
ErlingSJunior <erlingjunior@gmail.com>
2021-01-06 17:08:17 -0300

    Adicionado Import das PropTypes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
