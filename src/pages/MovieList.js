ebf07f0c1dea93d821385935c48447ff5ce20ecc
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-08 13:55:36 -0300

    segundo requisito

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+// import getMovies from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,21 +10,35 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      // getMovies: getMovies(),
+      loaded: false,
     }
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loaded } = this.state;
 
     // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
-        <div >Movie Card Library CRUD teste</div>
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
-      </div>
-    );
+    if (loaded) {
+      return (
+        <div data-testid="movie-list">
+          <div >Movie Card Library CRUD teste</div>
+          {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        </div>
+      );
+      } return <Loading />;
   }
+
+  componentDidMount() {
+    console.log("Did mount");
+    this.getMovies();
+  }
+ // como isso veio parar aqui desde o movieAPI? porque funciona?
+  async getMovies() {
+    const moviesAPI = await movieAPI.getMovies();
+    this.setState({ movies: moviesAPI, loaded: true });
+  }
+
 }
 
 export default MovieList;
