7ad17d56ca0230b7c7018c80e2064607f5e5fc1c
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-07 23:31:31 -0300

    Testando o teste do Git

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 // Projeto CRUD
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound}  from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <div>Movie Card Library CRUD</div>
+        <Route exact path="/" component={ MovieList }/>
+        <Route exact path="/movies/:id/edit" component={ EditMovie }/>
+        <Route exact path="/movies/:id" component={ MovieDetails }/>
+        <Route exact path="/movies/new" component={ NewMovie }/>
+        <Route component={ NotFound }/>
+      </Switch>
+    </Router>
   );
 }
 
