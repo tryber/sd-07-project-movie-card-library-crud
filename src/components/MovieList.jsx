dbadb32faae02b6076960f4abc40ef650872cbfa
André Ferreira <andrefpires1@gmail.com>
2021-01-09 14:14:16 -0300

    Requisito 1 | Teste data-testid - CC 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class MovieList extends Component {
   render() {
     return (
-      <div data-testid='movie-list'>
+      <div data-testid="movie-list">
         <h1>Movie List</h1>
       </div>
     );
