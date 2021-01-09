a7d68415f1b4b7761e080e83eb11507d63906011
André Ferreira <andrefpires1@gmail.com>
2021-01-09 16:45:02 -0300

    Requisito 3 | Colocando link ver detalhes em MovieCard, nome e sinopse

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { name, storyline } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <header>
+          <h1>Movie Card</h1>
+        </header>
+        <div>
+          <h2>{name}</h2>
+          <p>{storyline}</p>
+        </div>
+        <Link to={`movies/${id}`}>Ver detalhes</Link>
       </div>
     );
   }
