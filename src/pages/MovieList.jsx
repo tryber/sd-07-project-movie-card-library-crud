f1b6488bb2bf68a8cf651e3a1b1ac03187d5f6b1
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-01-09 20:17:56 -0300

    resolucao 6

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import * as movieAPI from '../services/movieAPI';
+
+class MovieList extends Component {
+  constructor() {
+    super();
+    this.fetchMovies = this.fetchMovies.bind(this);
+    this.state = {
+      movies: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    this.setState({ loading: true }, () => {
+      movieAPI.getMovies().then((movies) => {
+        this.setState({
+          loading: false,
+          movies,
+        });
+      });
+    });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    // Render Loading here if the request is still happening
+    if (loading) {
+      return <p>Carregando...</p>;
+    }
+
+    return (
+      <div data-testid="movie-list">
+        <div>
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        </div>
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
