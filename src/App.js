221259b06bef3b4bb8e5876440e0e6311fd8aee8
André Ferreira <andrefpires1@gmail.com>
2021-01-09 14:02:59 -0300

    Requisito 1 | Rotas - CC 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import NewMovie from './components/NewMovie';
@@ -11,11 +11,13 @@ function App() {
     <div>
       <h1>Movie Card Library CRUD</h1>
       <BrowserRouter>
-        <Route component={NotFound} />
-        <Route path='/movies/:id/edit' component={EditMovie} />
-        <Route path='/movies/new' component={NewMovie} />
-        <Route exact path='/movies/:id' component={MovieDetails} />
-        <Route exact path='/' component={MovieList} />
+        <Switch>
+          <Route component={NotFound} />
+          <Route path='/' component={MovieList} />
+          <Route path='/movies/:id' component={MovieDetails} />
+          <Route path='/movies/:id/edit' component={EditMovie} />
+          <Route path='/movies/new' component={NewMovie} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
