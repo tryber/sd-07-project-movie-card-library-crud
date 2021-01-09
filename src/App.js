f50c147f8412b56303690c5c8c3099f7f7af156e
André Ferreira <andrefpires1@gmail.com>
2021-01-09 14:30:10 -0300

    Requisito 1 | Excluindo componentes que criei no início e mundando importações - Esqueci de uma importação

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
-import EditMovie from './components/EditMovie';
+import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
 function App() {
