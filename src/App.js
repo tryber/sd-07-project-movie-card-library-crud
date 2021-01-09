23f6b07a52d263c629000432c4ae5bcf0809ec5e
André Ferreira <andrefpires1@gmail.com>
2021-01-09 14:00:08 -0300

    Requisito 1 | Rotas - CC 2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route component={NotFound} />
         <Route path='/movies/:id/edit' component={EditMovie} />
         <Route path='/movies/new' component={NewMovie} />
-        <Route path='/movies/:id' component={MovieDetails} />
+        <Route exact path='/movies/:id' component={MovieDetails} />
         <Route exact path='/' component={MovieList} />
       </BrowserRouter>
     </div>
