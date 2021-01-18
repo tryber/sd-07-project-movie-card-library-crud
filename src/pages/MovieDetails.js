df6abe9d2b83d6533182cc6cf6b0f3d6993d181e
Isabella Paz <bellapaz.iorp@gmail.com>
2021-01-18 16:39:52 -0300

    Movie Details - Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.goDeleteMovie = this.goDeleteMovie.bind(this);
+
+    this.state = {
+      movie: {},
+      status: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const { getMovie } = movieAPI;
+    await getMovie(id).then((movie) => {
+      this.setState({
+        movie,
+      });
+    });
+  }
+
+  goDeleteMovie() {
+    const { id } = this.props.match.params;
+    const { deleteMovie } = movieAPI;
+    deleteMovie(id);
+  }
+
+  render() {
+    const { status, movie } = this.state;
+    const { id } = this.props.match.params;
+    const {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+    } = movie;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={`../${imagePath}`} />
-        <p>{`Subtitle: ${subtitle}`}</p>
-        <p>{`Storyline: ${storyline}`}</p>
-        <p>{`Genre: ${genre}`}</p>
-        <p>{`Rating: ${rating}`}</p>
+        {
+          status === 'loading'
+            ? <Loading />
+            : (
+              <span>
+                <h2>{title}</h2>
+                <div>
+                  <img alt="Movie Cover" src={`../${imagePath}`} />
+                  <p>{`Subtítulo: ${subtitle}`}</p>
+                  <p>{`Sinopse: ${storyline}`}</p>
+                  <p>{`Gênero: ${genre}`}</p>
+                  <p>{`Avaliação: ${rating}`}</p>
+                </div>
+                <Link to={`${id}/edit`}>EDITAR</Link>
+                <Link to="/" onClick={this.goDeleteMovie}>DELETAR</Link>
+                <Link to="/">VOLTAR</Link>
+              </span>
+            )
+        }
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
