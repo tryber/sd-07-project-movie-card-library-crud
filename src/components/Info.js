f34aca43ce5a08ac3c0a3a6f522ba674d8f4515e
Thalys <thalysmatiascarrara@gmail.com>
2021-01-18 18:43:38 -0400

    requisito bonus

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,22 +1,40 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-export default function Info(props) {
-  const { imagePath, subtitle, title, storyline, genre, rating } = props.movie;
-  const { id } = props;
-  return (
-    <div>
-      <img alt="Movie Cover" src={`../${imagePath}`} />
-      <h1>{title}</h1>
-      <p>{`Subtitle: ${subtitle}`}</p>
-      <p>{`Storyline: ${storyline}`}</p>
-      <p>{`Genre: ${genre}`}</p>
-      <p>{`Rating: ${rating}`}</p>
-      <Link to={`/movies/${id}/edit`}>EDITAR</Link>
-      <Link to="/">VOLTAR</Link>
-    </div>
-  );
+import { Link } from 'react-router-dom';
+import { deleteMovie } from '../services/movieAPI';
+
+export default class Info extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteCard = this.deleteCard.bind(this);
+  }
+
+  async deleteCard() {
+    const idMovie = this.props.id;
+    return deleteMovie(idMovie);
+  }
+
+  render() {
+    const { imagePath, subtitle, title, storyline, genre, rating } = this.props.movie;
+    const { id } = this.props;
+    console.log(this.props.movie);
+
+    return (
+      <div>
+        <img alt="Movie Cover" src={`../${imagePath}`} />
+        <h1>{title}</h1>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to={'/'} onClick={this.deleteCard}>DELETAR</Link>
+        <Link to="/">VOLTAR</Link>
+      </div>
+    );
+  }
 }
 
 Info.propTypes = {
