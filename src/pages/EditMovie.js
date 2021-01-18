6c7c9add537c03e9ec72d2ae18294477fb9d72b0
Breenolf <blf@ic.ufal.br>
2021-01-18 16:33:30 -0300

    requirement 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loaded',
+      shouldRedirect: false,
+      movie: {},
+    };
+    this.fetchMovieAPI = this.fetchMovieAPI.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchMovieAPI();
+  }
+
+  fetchMovieAPI() {
+    this.setState({
+      status: 'loading',
+    }, async () => {
+      const { id } = this.props.match.params;
+      const movie = await movieAPI.getMovie(id);
+      this.setState({
+        movie,
+        status: 'loaded',
+      });
+    });
+  }
+
   handleSubmit(updatedMovie) {
+    this.setState({
+      shouldRedirect: true,
+    }, async () => {
+      await movieAPI.updateMovie(updatedMovie);
+      this.setState({
+        status: 'loaded',
+        shouldRedirect: true,
+      });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return (
+        <div>
+          <Loading />
+        </div>
+      );
     }
 
     return (
