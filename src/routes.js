702bd440bad40b9eb2927fface710c96a708102b
Eriquy Jhordan <eriquy@gmail.com>
2021-01-18 15:27:19 -0300

    codeclimate

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound  } from './pages/index';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {MovieList, MovieDetails, NewMovie, EditMovie, NotFound} from './pages/index';
 
 export default class Routes extends React.Component {
   render() {
     return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={ MovieList } />
-        <Route path='/movies/:id' component={ MovieDetails } />
-        <Route path='/movies/new' component={ NewMovie } />
-        <Route path='/movies/:id/edit' component={ EditMovie } />
-        <Route path='*' component={ NotFound } />
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
     );
