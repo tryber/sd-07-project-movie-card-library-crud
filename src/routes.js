6c77baa3e04191bb0ab2c6ad566af148bd888a84
Eriquy Jhordan <eriquy@gmail.com>
2021-01-18 16:26:48 -0300

    exact to routes

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ export default class Routes extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={MovieList} />
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/:id" component={MovieDetails} />
+          <Route exact path="/movies/new" component={NewMovie} />
+          <Route exact path="/movies/:id/edit" component={EditMovie} />
           <Route path="*" component={NotFound} />
         </Switch>
       </BrowserRouter>
