409fd21a4b4acf367733297cdedbdf151b0207ea
Jorge Alves <alvesdjorge@gmail.com>
2021-01-19 00:33:29 -0300

    requisito 1 code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
 import './App.css';
 import {
   MovieList,
@@ -16,7 +16,8 @@ function App() {
         <Route exact path="/" component={MovieList} />
         <Route exact path="/movies/new" component={NewMovie} />
         <Route path="/movies/:id/edit"
-          render={(props) => <EditMovie {...props} />} />
+          render={(props) => <EditMovie {...props} />}
+        />
         <Route path="/movies/:id"
           render={(props) => <MovieDetails {...props} />}
         />
