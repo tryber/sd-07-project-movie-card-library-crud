ce822fa67a03d42c566eb3f5291c82822f058681
Jorge Alves <alvesdjorge@gmail.com>
2021-01-19 01:27:26 -0300

    requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      Loading: false,
+    }
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.getSnapshotBeforeUpdate(id);
+  }
+
+  async get(id) {
+    const movie = await movieAPI.getMovie(id);
+    this.setState({ movie, Loading: true })
+  }
+
+  async deleteMovie(movieId) {
+    await movieAPI.deleteMovie(movieId);
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const {
+      id,
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle
+    } = this.state.movie;
+    const loading = this.state.loading;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={`../${imagePath}`} />
+        <p>{`Title: ${title}`}</p>
         <p>{`Subtitle: ${subtitle}`}</p>
         <p>{`Storyline: ${storyline}`}</p>
         <p>{`Genre: ${genre}`}</p>
         <p>{`Rating: ${rating}`}</p>
+        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <button onClick={() => this.deleteMovie(id)}>
+          <link to="/">DELETAR</link>
+        </button>
       </div>
+    ) : (
+      <Loading />
     );
   }
+}
+
+MovieDetails.PropTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired
 }
 
 export default MovieDetails;
