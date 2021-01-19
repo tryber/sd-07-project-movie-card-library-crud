b3a2893ed9c9a17f7f32cbad6dca57fcf3c4449f
Jorge Alves <alvesdjorge@gmail.com>
2021-01-19 00:23:42 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Switch } from 'react-router-dom';
 import './App.css';
+import {
+  MovieList,
+  MovieDetails,
+  NewMovie,
+  NotFound,
+  EditMovie,
+} from './pages';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit"
+          render={(props) => <EditMovie {...props} />} />
+        <Route path="/movies/:id"
+          render={(props) => <MovieDetails {...props} />}
+        />
+        <Route path="/" component={NotFound} />
+      </Switch>
     </Router>
   );
 }
